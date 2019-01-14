f499afd98fb111fddc81b9e4aaf74f1ae27845cd
visokoo <ta.vivian@gmail.com>
2019-01-14 12:41:08 -0800

    Checking in lesson1 assignments

---
+++ students/visokoo/lesson1_assignments/break_me.py
@@ -0,0 +1,29 @@
+'''
+----------------------------------------------------
+Assignment: Explore Errors
+Author: visokoo | Created: 1/13/2019
+ChangeLog: 1/13/2019, Created file 
+
+Create a script with four exceptions
+----------------------------------------------------
+
+'''
+
+def name_error(x):
+  print(y)
+
+#name_error(x)
+
+def value_error(x):
+  int(x)
+  print(x, "blah")
+
+#value_error("duh")
+
+def zero_division(x):
+  return x / 0
+
+#zero_division(2)
+
+#def syntax_error(x)
+#  print(x)
